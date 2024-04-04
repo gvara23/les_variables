1410886697304d61b7861d48bf1f076a1228b8a8
Heidy <heidyguevara23@hotmail.com>
2024-04-04 19:07:17 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 2;
+
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("PERMUTATION");
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("a = " + a);
+
+            Console.ReadKey();
+        }
+    }
+}
