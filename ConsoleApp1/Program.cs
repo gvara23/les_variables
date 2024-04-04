f231397e5943b2d4ec2e2a81a0915a541d055e8d
Heidy <heidyguevara23@hotmail.com>
2024-04-04 19:13:06 +0200

    little hange of the code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,10 +10,6 @@
             Console.WriteLine("a = " + a);
             Console.WriteLine("b = " + b);
 
-            int temp = a;
-            a = b;
-            b = temp;
-
             Console.WriteLine("PERMUTATION");
             Console.WriteLine("b = " + b);
             Console.WriteLine("a = " + a);
